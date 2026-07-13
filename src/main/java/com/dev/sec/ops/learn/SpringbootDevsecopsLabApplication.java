8d3698bae30537c082e6ebc404b4a975f8bff210
sjcebhushan <shashibhushansjce@gmail.com>
2026-07-13 09:14:39 +0530

    First Commit

---
+++ src/main/java/com/dev/sec/ops/learn/SpringbootDevsecopsLabApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sec.ops.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevsecopsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevsecopsLabApplication.class, args);
+	}
+
+}
